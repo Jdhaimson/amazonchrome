62f92f84fa15e8287c27cc11e2bbc9660175cc04
Josh Haimson <jhaimson@flatiron.com>
2017-07-01 18:18:28 -0400

    Add analytics

--- chromeextension/index.js
+++ chromeextension/index.js
@@ -53,3 +53,24 @@ $(function() {
     $(".buy-btn a").text(millenialInfo);
 
 });
+
+// Add google analytics code
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-101972688-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+document.onLoad = function() {
+    function trackButton(e) {
+        _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+    };
+
+    var buttons = document.querySelectorAll('button');
+    var button = $("#buy")[0];
+    button.addEventListener('click', trackButton);
+};
